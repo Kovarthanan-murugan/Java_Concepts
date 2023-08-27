77dfe1708cc49c04e61601645e3118c44311ceb1
Kovarthanan Murugan <kv948064@dal.ca>
2023-08-27 14:30:08 -0300

    Add inheritance

---
+++ src/main/java/org/inheritance/Animal.java
@@ -0,0 +1,15 @@
+package org.inheritance;
+
+public class Animal{
+
+     int a = 10;
+    public void makeSound(){
+
+        System.out.println("making animal sound"+a);
+
+    }
+
+    public void commonSound(){
+        System.out.println("commonsound");
+    }
+}
---
+++ src/main/java/org/inheritance/Cat.java
@@ -0,0 +1,8 @@
+package org.inheritance;
+
+public class Cat extends Animal{
+    public void makeSound(){
+        System.out.println("making cat sound"+a);
+    }
+
+}
